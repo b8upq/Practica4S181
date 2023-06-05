3b738f23d9e5f361d623615e2ccd040bc4097212
Jesus Almaguer <111038018+b8upq@users.noreply.github.com>
2023-06-04 22:49:12 -0600

    Se arreglo la base de datos, se creo las funciones de consultar y actualizar

--- index.py
+++ index.py
@@ -1,39 +1,117 @@
-import sqlite3
 from operations import *
 
-def opcion1():
-    idR = int(input("Ingresa el ID de tu bebida: "))
-    nombreR = str(input("Ingresa el nombre de la bebida: "))
-    clasificacionR = str(input("Ingresa la clasificación de la bebida: "))
-    marcaR = str(input("Ingresa la marca de la bebida: "))
-    precioR = float(input("Ingresa el precio de tus bebida: "))
+drinks = Almacen()
 
-    drinks = Almacen()
+class Menu:
+    def opcion1():
+        idR = int(input("Ingresa el ID de tu bebida: "))
+        nombreR = str(input("Ingresa el nombre de la bebida: "))
+        clasificacionR = str(input("Ingresa la clasificación de la bebida: "))
+        marcaR = str(input("Ingresa la marca de la bebida: "))
+        precioR = float(input("Ingresa el precio de tus bebida: "))
 
-    drinks.altaBD(idR,nombreR,clasificacionR,marcaR,precioR)
-    
-def opcion2():
-    print("Has escogido la opción 2.")    
+        drinks.altaBD(idR,nombreR,clasificacionR,marcaR,precioR)
+        
+    def opcion2():
+        print("Has escogido la opción 2.")    
 
-def opcion3():
-    print("Has escogido la opción 3.")  
-    
-def opcion4():
-    print("Has escogido la opción 4.")  
+    def opcion3():
+        print("Selecciona que campo deseas consultar: ")
+        print("1. ID, 2.NOMBRE, 3. CLASIFICACIÓN, 4. MARCA, 5. PRECIO, 6. Escribe '0' para salir")
+        def search1():
+            sID = int(input("Ingresa el id:"))
+            drinks.searchID(sID)
+        
+        def search2():
+            searchnombre = str(input("Ingresa el nombre:"))
+            drinks.searchNOMBRE(searchnombre)
+
+        def search3():
+            searchCLAS = str(input("Ingresa el clasificación:"))
+            drinks.searchCLAS(searchCLAS)
+            
+        def search4():
+            searchMARCA = str(input("Ingresa el marca:"))
+            drinks.searchMARCA(searchMARCA)
+            
+        def search5():
+            searchPRECIO = float(input("Ingresa el precio:"))
+            drinks.searchPRECIO(searchPRECIO)
+            
+        opciones = {
+            1: search1,
+            2: search2,
+            3: search3,
+            4: search4,
+            5: search5
+        }
+        
+        while True:
+            seleccion = int(input("Elige una opción de consulta: "))
+            if seleccion == 0:
+                    print("SALIENDO DEL MENU DE CONSULTAS....")
+                    print("-----Estas de nuevo en el menú principal.-----")
+                    break
+            if seleccion in opciones:
+                opciones[seleccion]()   
+            else:
+                print("Opción inválida. Por favor, elige nuevamente.")
+        
+    def opcion4():
+        print("Selecciona que campo deseas actualizar: ")
+        print("1.NOMBRE, 2. CLASIFICACIÓN, 3. MARCA, 4. PRECIO, 5. Escribe '0' para salir")
+        def update1():
+            uID = int(input("Ingresa el id:"))
+            unombre = str(input("Ingresa el nombre:"))
+            drinks.updateNOMBRE(uID,unombre)
+        
+        def update2():
+            uID = int(input("Ingresa el id:"))
+            uCLAS = str(input("Ingresa el clasificación:"))
+            drinks.updateCLAS(uID,uCLAS)
+
+        def update3():
+            uID = int(input("Ingresa el id:"))
+            uMARCA = str(input("Ingresa el marca:"))
+            drinks.updateMARCA(uID,uMARCA)
+            
+        def update4():
+            uID = int(input("Ingresa el id:"))
+            uPRECIO = float(input("Ingresa el precio:"))
+            drinks.updatePRECIO(uID,uPRECIO)
+        
+            
+        opciones = {
+            1: update1,
+            2: update2,
+            3: update3,
+            4: update4
+        }
+        
+        while True:
+            seleccion = int(input("Elige una opción de actualización: "))
+            if seleccion == 0:
+                    print("\nSALIENDO DEL MENU DE ACTUALIZACIÓN....")
+                    print("\n-----Estas de nuevo en el menú principal.-----")
+                    break
+            if seleccion in opciones:
+                opciones[seleccion]()   
+            else:
+                print("Opción inválida. Por favor, elige nuevamente.")  
     
     
 print("------ MENU ------")
-print("1. Opción 1")
-print("2. Opción 2")
-print("3. Opción 3")
-print("4. Opción 4")
+print("1. Genera un registro.")
+print("2. Elimina un registro.")
+print("3. Realiza una consulta.")
+print("4. Realiza una actualización de datos.")
 print("0. Salir")
 
 opciones = {
-    1: opcion1,
-    2: opcion2,
-    3: opcion3,
-    4: opcion4
+    1: Menu.opcion1,
+    2: Menu.opcion2,
+    3: Menu.opcion3,
+    4: Menu.opcion4
 }
 
 while True:
--- operations.py
+++ operations.py
@@ -22,5 +22,109 @@ class Almacen:
         conexion.commit()
         conexion.close()
         print("¡Registro exitoso!")
-
         
+    def searchID(self, id):
+        conexion = sqlite3.connect('Almacen_Bebidas.db')
+        cursor = conexion.cursor()
+        instruccion = f"SELECT * FROM Bebidas WHERE id = {id}"
+        cursor.execute(instruccion)
+        datos = cursor.fetchall()
+        conexion.commit()
+        conexion.close()
+        
+        if (datos == []):
+            print("No existen articulos con ese ID.")  
+        else:
+            print(datos)
+        
+    def searchNOMBRE(self, nombre):
+        conexion = sqlite3.connect('Almacen_Bebidas.db')
+        cursor = conexion.cursor()
+        instruccion = f"SELECT * FROM Bebidas WHERE nombre like '{nombre}%'"
+        cursor.execute(instruccion)
+        datos = cursor.fetchall()
+        conexion.commit()
+        conexion.close()
+        
+        if (datos == []):
+            print("No existen articulos con ese nombre.")  
+        else:
+            print(datos)
+        
+    def searchCLAS(self, clasificacion):
+        conexion = sqlite3.connect('Almacen_Bebidas.db')
+        cursor = conexion.cursor()
+        instruccion = f"SELECT * FROM Bebidas WHERE clasificacion like '{clasificacion}%'"
+        cursor.execute(instruccion)
+        datos = cursor.fetchall()
+        conexion.commit()
+        conexion.close()
+        
+        if (datos == []):
+            print("No existen articulos con ese clasificación.")  
+        else:
+            print(datos)
+        
+    def searchMARCA(self, marca):
+        conexion = sqlite3.connect('Almacen_Bebidas.db')
+        cursor = conexion.cursor()
+        instruccion = f"SELECT * FROM Bebidas WHERE marca like '{marca}'"
+        cursor.execute(instruccion)
+        datos = cursor.fetchall()
+        conexion.commit()
+        conexion.close()
+        
+        if (datos == []):
+            print("No existen articulos con ese marca.")  
+        else:
+            print(datos)
+        
+    def searchPRECIO(self, precio):
+        conexion = sqlite3.connect('Almacen_Bebidas.db')
+        cursor = conexion.cursor()
+        instruccion = f"SELECT * FROM Bebidas WHERE precio = {precio}"
+        cursor.execute(instruccion)
+        datos = cursor.fetchall()
+        conexion.commit()
+        conexion.close()
+        
+        if (datos == []):
+            print("No existen articulos con ese precio.")  
+        else:
+            print(datos)
+
+    def updateNOMBRE(self, id, nombre):
+            conexion = sqlite3.connect('Almacen_Bebidas.db')
+            cursor = conexion.cursor()
+            instruccion = f"UPDATE Bebidas SET nombre = '{nombre}' WHERE id = {id}"
+            cursor.execute(instruccion)
+            conexion.commit()
+            conexion.close()        
+            print("Se ha actualizado el nombre correctamente.")
+            
+    def updateCLAS(self, id, clasificacion):
+            conexion = sqlite3.connect('Almacen_Bebidas.db')
+            cursor = conexion.cursor()
+            instruccion = f"UPDATE Bebidas SET clasificacion = '{clasificacion}' WHERE id = {id}"
+            cursor.execute(instruccion)
+            conexion.commit()
+            conexion.close()        
+            print("Se ha actualizado el clasificación correctamente.")
+            
+    def updateMARCA(self, id, marca):
+            conexion = sqlite3.connect('Almacen_Bebidas.db')
+            cursor = conexion.cursor()
+            instruccion = f"UPDATE Bebidas SET marca = '{marca}' WHERE id = {id}"
+            cursor.execute(instruccion)
+            conexion.commit()
+            conexion.close()        
+            print("Se ha actualizado el marca correctamente.")
+            
+    def updatePRECIO(self, id, precio):
+            conexion = sqlite3.connect('Almacen_Bebidas.db')
+            cursor = conexion.cursor()
+            instruccion = f"UPDATE Bebidas SET precio = {precio} WHERE id = {id}"
+            cursor.execute(instruccion)
+            conexion.commit()
+            conexion.close()        
+            print("Se ha actualizado el precio correctamente.")
